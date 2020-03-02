1e519dd6678728c40121063bffbb71c20e9171fa
GD-Prasad <g.d.prasad004@gmail.com>
2020-03-02 10:45:48 -0600

    js

--- challenge.js
+++ challenge.js
@@ -1,12 +1,11 @@
-function Challenge()
-{
-     const Count=parseInt(document.querySelector('#coc').value)
-     const avgStudent=parseInt(document.querySelector('#aspc').value)
-     
-     const EstimatedStudent=Count*avgStudent;
-    
+function Challenge() {
+    const Count = parseInt(document.querySelector('#coc').value)
+    const avgStudent = parseInt(document.querySelector('#aspc').value)
 
-     
-     document.querySelector('#EstimatedStudent').innerHTML=`${EstimatedStudent}`
+    const EstimatedStudent = Count * avgStudent;
+
+
+
+    document.querySelector('#EstimatedStudent').innerHTML = `${EstimatedStudent}`
 }
-     document.querySelector('#calculate').addEventListener('click',Challenge)
+document.querySelector('#calculate').addEventListener('click', Challenge)
